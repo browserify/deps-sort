65b8d6b72e72832116b267d89cd23a93bd5a3c71
James Halliday <mail@substack.net>
2013-07-08 22:57:21 -0700

    bin script

---
+++ bin/cmd.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var sort = require('../')();
+var JSONStream = require('JSONStream');
+var parse = JSONStream.parse([ true ]);
+var stringify = JSONStream.stringify();
+
+process.stdin.pipe(parse).pipe(sort).pipe(stringify).pipe(process.stdout);
